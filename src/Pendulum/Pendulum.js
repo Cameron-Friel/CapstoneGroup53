691e34124d9fe50625dfbf3b9e60c0b2c55ac0a2
Cameron-Friel <frielc@oregonstate.edu>
2019-01-24 11:26:04 -0800

    Changes Pendulum.js from closure to class.

--- src/Pendulum/Pendulum.js
+++ src/Pendulum/Pendulum.js
@@ -1,67 +1,107 @@
 'use strict';
 
-let Pendulum = (function() {
-  return {
-    pendulumAngle: null,
-    pendulumHeight: null,
-    pendulumWeight: null,
-    pendulumString: null,
+/**
+  * @class Pendulum
+ * The Pendulum module is in charge of keeping tack of the pendulum bodies and properties within the world.
+*/
 
-    /*
-      * Returns the angle of the pendulum
-      * @returns {Int} pendulumAngle
-    */
+class Pendulum {
+  constructor() {
+    this._pendulumBody = null;
+    this._pendulumAngle = null;
+    this._pendulumHeight = null;
+    this._pendulumString = null;
+  }
 
-    getPendulumAngle: function() {
-      return this.pendulumAngle;
-    },
+  /**
+    * Sets the body of the pendulum
+  */
 
-    /*
-      * Updates the value of pendulumAngle
-      * @param {Int} angle - the current angle of the pendulum
-    */
+  set pendulumBody(body) {
+    this._pendulumBody = body;
+  }
 
-    setPendulumAngle: function(angle) {
-      this.pendulumAngle = angle;
-    },
+  /**
+    * Returns the body of the pendulum
+    * @returns {Bodies} _pendulumBody
+  */
 
-    /*
-      * Calculates the arctangent of a line given two coordinates
-      * @param {Int} firstPoint - the first coordinate given as (x, y) as its datamembers
-      * @param {Int} secondPoint - the second coordinate given as (x, y) as its datamembers
-      * @returns {Int} angle - the angle of the line
-    */
+  get pendulumBody() {
+    return this._pendulumBody;
+  }
 
-    calculateAngle: function(firstPoint, secondPoint) {
-      return Math.abs(Math.round(Math.atan2(firstPoint.y - secondPoint.y, firstPoint.x - secondPoint.y) * 180 / Math.PI));
-    },
+  /**
+    * Sets the body of the string
+    * @returns {Bodies} _pendulumString
+  */
 
-    /*
-      * Updates the angle of the pendulum to the user
-    */
+  set pendulumString(string) {
+    this._pendulumString = string;
+  }
 
-    displayPendulumAngle: function() {
-      document.getElementById('pendulum-angle').textContent = 'Angle: ' + this.pendulumAngle;
-    },
+  /**
+    * Returns the body of the string
+    * @returns {Body} _pendulumString
+  */
 
-    /*
-      * Returns the height of the pendulum measured from the bottom of the canvas to the bottom of the pendulum
-      * @returns {Int} pendulumHeight
-    */
+  get pendulumString() {
+    return this._pendulumString;
+  }
 
-    getPendulumHeight: function() {
-      return this.pendulumHeight;
-    },
+  /**
+    * Calculates the arctangent of a line given two coordinates
+    * @param {Int} firstPoint - the first coordinate given as (x, y) as its datamembers
+    * @param {Int} secondPoint - the second coordinate given as (x, y) as its datamembers
+    * @returns {Int} angle - the angle of the line
+  */
 
-    /*
-      * Updates the value of pendulumHeight
-      * @param {Int} height - the current height of the pendulum from the bottom of the canvas
-    */
+  calculateAngle(firstPoint, secondPoint) {
+    return Math.abs(Math.round(Math.atan2(firstPoint.y - secondPoint.y, firstPoint.x - secondPoint.y) * 180 / Math.PI));
+  }
 
-    setPendulumHeight: function(height) {
-      this.pendulumHeight = height
-    },
-  };
-})();
+  /**
+    * Updates the angle of the pendulum to the user
+  */
+
+  displayPendulumAngle() {
+    document.getElementById('pendulum-angle').textContent = 'Angle: ' + this.pendulumAngle;
+  }
+
+  /**
+    * Updates the value of _pendulumAngle
+    * @param {Int} angle - the current angle of the pendulum
+  */
+
+  set pendulumAngle(angle) {
+    this._pendulumAngle = angle;
+  }
+
+  /**
+    * Returns the angle of the pendulum
+    * @returns {Int} _pendulumAngle
+  */
+
+  get pendulumAngle() {
+    return this._pendulumAngle;
+  }
+
+  /**
+    * Updates the value of _pendulumHeight
+    * @param {Int} height - the current height of the pendulum from the bottom of the canvas
+  */
+
+  set pendulumHeight(height) {
+    this._pendulumHeight = height;
+  }
+
+  /**
+    * Returns the height of the pendulum measured from the bottom of the canvas to the bottom of the pendulum
+    * @returns {Int} _pendulumHeight
+  */
+
+  get pendulumHeight() {
+    return this._pendulumHeight;
+  }
+}
 
 module.exports = Pendulum;
