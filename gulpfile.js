8b604ce4c357cb6bed2492bd98178764755a0d76
Cameron-Friel <frielc@oregonstate.edu>
2019-02-07 21:41:15 -0800

    Implements generic build, clean, and minify for building project

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,48 @@
 let gulp = require('gulp');
+let terser = require('gulp-terser');
+let clean = require('gulp-clean');
+let fs = require('fs');
 let browserify = require('browserify');
 let source = require('vinyl-source-stream');
 
-/*
-  * Builds the Pendulum case 1 source files
+/**
+  * Minifies all js files in the build folder
 */
 
-gulp.task('build-case1', async () => {
-  browserify('./src/Pendulum/case1/Sketch.js').bundle().pipe(source('bundle.js')).pipe(gulp.dest('./src/Pendulum/case1'));
+gulp.task('minify', () => {
+  return gulp.src('./build/*.js').pipe(terser()).pipe(gulp.dest('./build'), {mode: 0777});
+});
+
+
+/**
+  * Builds a pendulum case to the build folder
+  * Takes a command line argument --case [case] where case is a folder name
+*/
+
+gulp.task('build', async () => {
+  if (process.argv.indexOf('--case') !== -1) {
+    let folder = process.argv[process.argv.indexOf('--case') + 1];
+
+    browserify(`./src/Pendulum/${folder}/Sketch.js`).bundle().pipe(source('bundle.js')).pipe(gulp.dest('./build'), {mode: 0777});
+    gulp.src(`./src/Pendulum/${folder}/**/*.css`).pipe(gulp.dest('./build'), {mode: 0777});
+    gulp.src(`./src/Pendulum/${folder}/**/*.html`).pipe(gulp.dest('./build'), {mode: 0777});
+  }
+  else {
+    console.log('Error: The folder to build was not specified. Enter the command gulp build-case --case [Your Case].');
+  }
+});
+
+/**
+  * Cleans the build directory
+*/
+
+gulp.task('clean', async () => {
+  fs.access('./build', (error) => {
+    if (error) {
+      console.log('Error: ./build does not exist.');
+    }
+    else {
+      gulp.src('./build', {read: false}).pipe(clean(), {mode: 0777});
+    }
+  });
 });
